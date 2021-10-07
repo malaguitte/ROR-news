6fa3836a500a56b3cc1b553f5a083025ab9fbcf6
Anderson Malaguitte <upmalagutti@gmail.com>
2021-10-08 01:08:12 +0200

    Basic user auth working

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,20 @@
 class HomeController < ApplicationController
+  
   def index
+    p params;
   end
+
+  def login
+    @isLoginSuccessful = false;
+    usernameReceived = params["username"];
+    if usernameReceived
+      user = User.find_by_username(usernameReceived)
+      if user != nil
+        # Tries to authenticate the user with the password received
+        @isLoginSuccessful = user.authenticate(params["password"]);
+      end
+    end
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :users
   get "home/index"
+  get "home/login"
+  post "home/login"
   post "home/index"
   root "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
