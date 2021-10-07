bb44d4e0f2cb92b66ddcf060f090f6ae868132a1
Anderson Malaguitte <upmalagutti@gmail.com>
2021-10-08 00:04:35 +0200

    Displaying some rss news on the home page

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,10 @@
 module HomeHelper
+  def articles(query)
+    require "rss";
+    require "open-uri";
+    url = "https://news.google.com/rss/search?cf=all&hl=en-US&pz=1&q=#{query}&gl=US&ceid=US:en";
+    open(url) do |rss|
+      RSS::Parser.parse(rss)
+    end
+  end
 end
