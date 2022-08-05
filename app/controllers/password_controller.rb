ae9336bb188b2d706385f735bf220a0257fe5dea
Anderson <upmalagutti@gmail.com>
2022-08-05 19:38:48 -0400

    small refactor

--- app/controllers/password_controller.rb
+++ app/controllers/password_controller.rb
@@ -1,36 +1,36 @@
 class PasswordController < ApplicationController
   
   def reset
-    token_received = request.query_parameters["token"] or params["token"] or not_found;
+    token_received = request.query_parameters['token'] || params['token'] || not_found
     # Tries to find the user based on the token received.
-    @user = User.find_by_reset(token_received) or not_found;
-    password_received = params["password"]
-    if password_received
-      # Updates the password & saves the user record.
-      @user.password = password_received;
-      @user.save;
-      render plain: "Successfully reset password";
-    end
+    @user = User.find_by_reset(token_received) || not_found
+    password_received = params['password']
+    return unless password_received
+
+    # Updates the password & saves the user record.
+    @user.password = password_received
+    @user.save
+    render plain: 'Successfully reset password'
   end
 
   def forgot
-    email_received = params["email"]
+    email_received = params['email']
     if email_received
-      user = User.find_by_email(email_received);
+      user = User.find_by_email(email_received)
       if user
         # Generate a 15 digit secure token
-        token = SecureRandom.hex(15);
+        token = SecureRandom.hex(15)
         # Assign it to the user & saves the record.
-        user.reset = token;
-        user.save;
-        ResetMailer.with(user: user).reset_password.deliver_now;
+        user.reset = token
+        user.save
+        ResetMailer.with(user: user).reset_password.deliver_now
       end
-      render plain: "A link to reset the password has been sent to your email if it exists";
+      render plain: 'A link to reset the password has been sent to your email if it exists'
     end
   end
 
   def not_found
-    raise ActionController::RoutingError.new("Not Found");
+    raise ActionController::RoutingError.new('Not Found')
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,30 @@
 class UsersController < ApplicationController
   
   def index
-    @users = User.all;
+    @users = User.all
   end
 
   def new
-    @user = User.new;
+    @user = User.new
   end
 
   def create
-    @user = User.new(user_params);
+    @user = User.new(user_params)
     if @user.save
-      flash.alert = "User created successfully.";
-      redirect_to @user;
+      flash[:alert] = 'User created successfully.'
+      redirect_to @user
     else 
-      flash.alert = "Error creating new user";
-      redirect_to new_user_path, alert: @user.errors.full_messages;
+      flash[:alert] = "Error creating new user";
+      redirect_to new_user_path, alert: @user.errors.full_messages
     end
   end
 
   def user_params
-    params.require(:user).permit(:username, :email, :password, :salt, :encrypted_password);
+    params.require(:user).permit(:username, :email, :password, :salt, :encrypted_password)
   end
 
   def show
-    @user = User.find(params[:id]);
+    @user = User.find(params[:id])
   end
 
 end
