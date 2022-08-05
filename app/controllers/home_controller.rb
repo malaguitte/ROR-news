6ccdf6c47fe5c9cd9c47b89b49f91863cc712110
Anderson <upmalagutti@gmail.com>
2022-08-05 19:45:00 -0400

    refactor home_controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,38 +4,31 @@ class HomeController < ApplicationController
 
   def index
     query = params[:query]
-    if query
-      @articles = helpers.get_articles(query);
-    end
+    @articles = helpers.fetch_articles(query: query) if query.present?
     # Caches the result here, so we can use it within the view file
     # It avoids doing many searches in our database
-    @is_user_logged_in = is_user_logged_in?
+    @is_user_logged_in = user_logged_in?
   end
 
   def login
-    is_login_successfull = false;
-    username_received = params["username"];
-    if username_received
-      user = User.find_by_username(username_received)
-      if user != nil
-        # Tries to authenticate the user with the password received
-        is_login_successfull = user.authenticate(params["password"]);
-        if is_login_successfull
-          session[:user_id] = user.id;
-          redirect_to root_path;
-        else
-          fail_login
-        end
-      else
-        fail_login
-      end
-    end
+    username_received = params['username']
+    fail_login unless username_received
+
+    user = User.find_by_username(username_received)
+    fail_login unless user.present?
+
+    # Tries to authenticate the user with the password received
+    success = user.authenticate(params['password'])
+    fail_login unless success
+
+    session[:user_id] = user.id
+    redirect_to root_path
   end
 
   def fail_login
     # flash alert so we can keep the alert for the next view after the redirect
-    flash.alert = "Username or password incorrect!";
-    redirect_to "/home/login";
+    flash.alert = 'Username or password incorrect!'
+    redirect_to '/home/login'
   end
 
   def logout
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,8 +1,8 @@
 module HomeHelper
-  def get_articles(query)
-    require "rss";
-    require "open-uri";
-    url = "https://news.google.com/rss/search?cf=all&hl=en-US&pz=1&q=#{query}&gl=US&ceid=US:en";
+  def fetch_articles(query:)
+    require 'rss'
+    require 'open-uri'
+    url = "https://news.google.com/rss/search?cf=all&hl=en-US&pz=1&q=#{query}&gl=US&ceid=US:en"
     open(url) do |rss|
       RSS::Parser.parse(rss)
     end
