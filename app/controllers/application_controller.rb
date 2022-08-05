02bb315d6ed38e1c458c044bd243606fbca3b7f8
Anderson <upmalagutti@gmail.com>
2022-08-05 19:39:05 -0400

    improved method naming in the application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,19 +4,19 @@ class ApplicationController < ActionController::Base
 
   # redirects the user if not logged in
   before_action :authorized
-  helper_method :get_current_user
-  helper_method :is_user_logged_in?
+  helper_method :current_user
+  helper_method :user_logged_in?
 
-  def get_current_user
-    return User.find_by_id(session[:user_id])
+  def current_user
+    User.find_by_id(session[:user_id])
   end
 
-  def is_user_logged_in?
-    return get_current_user != nil
+  def user_logged_in?
+    current_user != nil
   end
 
   def authorized
-    redirect_to "/home/login" unless is_user_logged_in?
+    redirect_to "/home/login" unless user_logged_in?
   end
 
 end
